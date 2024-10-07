9f3df37612acbd321a20393a1beccff364fb5c61
madchip777 <51477281+madchip777@users.noreply.github.com>
2024-10-07 23:17:35 +0200

    utilisation tview, et images

--- src/ProjectRed/main.go
+++ src/ProjectRed/main.go
@@ -1,28 +1,54 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"image/png"
 	"os"
-	"os/exec"
-	"runtime"
-	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
+const (
+	icon_humain = "ressource/icon_humain.png"
+)
+
 func main() {
 	redColor := tcell.NewRGBColor(255, 0, 0)
-	run := true
-	box := tview.NewBox().SetBorder(true).SetTitle("[ RED_PROJECT_ULTIMATE ]")
-	box.SetBorderColor(redColor)
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+
+	IMGbyte, err := os.ReadFile(icon_humain)
+	if err != nil {
 		panic(err)
 	}
-	for !run {
-		clearCmd()
 
-		time.Sleep(time.Second / 24)
+	app := tview.NewApplication()
+	image := tview.NewImage()
+	//print(string(IMGbyte))
+	b, err2 := base64.StdEncoding.DecodeString(string(IMGbyte[:]))
+	if err2 != nil {
+		panic(err2)
 	}
+
+	graphics, err3 := png.Decode(bytes.NewReader(b))
+	if err3 != nil {
+		panic(err3)
+	}
+
+	image.SetImage(graphics)
+
+	box := tview.NewBox().SetBorder(true).SetTitle("[ RED_PROJECT_ULTIMATE ]")
+	box.SetBorderColor(redColor)
+
+	if err4 := app.SetRoot(image, true).Run(); err4 != nil {
+		panic(err4)
+	}
+	// run := true
+	// for !run {
+	// 	clearCmd()
+
+	// 	time.Sleep(time.Second / 24)
+	// }
 
 }
 
@@ -30,14 +56,14 @@ func getArgs() []string {
 	return os.Args[1:]
 }
 
-func clearCmd() {
-	if runtime.GOOS == "windows" {
-		cmd := exec.Command("cmd", "/c", "cls")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	} else {
-		cmd := exec.Command("clear")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
-}
+// func clearCmd() {
+// 	if runtime.GOOS == "windows" {
+// 		cmd := exec.Command("cmd", "/c", "cls")
+// 		cmd.Stdout = os.Stdout
+// 		cmd.Run()
+// 	} else {
+// 		cmd := exec.Command("clear")
+// 		cmd.Stdout = os.Stdout
+// 		cmd.Run()
+// 	}
+// }
