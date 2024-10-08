5608b3754a27e207d893d55bdf4b9430568a565b
DannyBrando <ziblamaker@gmail.com>
2024-10-08 14:57:42 +0200

    Merged with Main (mod + sum aussi)

--- src/main.go
+++ src/main.go
@@ -1,34 +1,12 @@
 package main
 
 import (
-	"PPR/InventoryTool"
+	PPR "PPR/Scene"
 	"os"
 )
 
 func main() {
-	var crafts map[string]map[string]int
-	InventoryTool.GetCraftList(&crafts, "data/crafts.json")
-
-	var inv InventoryTool.Inventory = InventoryTool.InitInventory()
-	var player map[string]interface{} = InventoryTool.InitPlayer()
-	InventoryTool.AddGoldToPlayer(&player, 5)
-
-	InventoryTool.AddItemToInventory(&inv, "O_WolfPelt", 2)
-	InventoryTool.AddItemToInventory(&inv, "O_TrollSkin", 2)
-	InventoryTool.AddItemToInventory(&inv, "O_Plume", 2)
-	InventoryTool.AddItemToInventory(&inv, "O_BoarLeather", 2)
-	InventoryTool.AddItemToInventory(&inv, "EC_Mage", 1)
-
-	InventoryTool.Craft("EC_Adventurer", crafts, &inv, &player)
-
-	InventoryTool.HurtPlayer(&player, 99)
-	InventoryTool.EquipPlayerWith(&player, "EC_Adventurer", &inv)
-	InventoryTool.PrintInventory(inv)
-	InventoryTool.PrintPlayer(player)
-	print("\n\n\n\n")
-	InventoryTool.EquipPlayerWith(&player, "EC_Mage", &inv)
-	InventoryTool.PrintInventory(inv)
-	InventoryTool.PrintPlayer(player)
+	PPR.CreatePlayerWindow()
 
 }
 
