31a48a7b64c10b5ee8eeb6e7173c062fbd4d90b2
Maxime Cordonnier <mcordonn9277@gmail.com>
2024-10-09 19:26:52 +0200

    ajout fonctions de vente/achat

--- src/InventoryTool/InventoryTool.go
+++ src/InventoryTool/InventoryTool.go
@@ -16,7 +16,7 @@ type Inventory = map[string]int
 // |					Gestion data files                  |
 // |========================================================|
 
-// Ouvre le fichier json 'filepath' et le converti en variable lisible par golang
+// Ouvre le fichier json 'filepath' et le converti en variable lisible par golang dans 'res'
 func GetCraftList(res *map[string]map[string]int, filepath string) bool {
 	//Retourne 0 si aucune erreur n'a été rencontré lors du dépactage du json. Sinon retourne 1
 	file, err1 := os.ReadFile(filepath)
@@ -26,7 +26,8 @@ func GetCraftList(res *map[string]map[string]int, filepath string) bool {
 	return false
 }
 
-// Ouvre le fichier json 'filepath' et le converti en variable lisible par golang et converti les nombres à virgules en nombres entier.
+// Ouvre le fichier json 'filepath' et le converti en variable lisible par golang dans 'res',
+// et converti les nombres à virgules en nombres entier.
 func GetItemList(res *map[string]map[string]interface{}, filepath string) bool {
 	file, err1 := os.ReadFile(filepath)
 	if err1 != nil || json.Unmarshal(file, res) != nil {
@@ -47,14 +48,26 @@ func GetItemList(res *map[string]map[string]interface{}, filepath string) bool {
 	return false
 }
 
-// Juste un alias pour simplifier la compréhension du code au main car les fonctions marchent de la même manière
+// Juste un alias de GetItemList pour simplifier la compréhension du code au main,
+// car les fonctions marchent de la même manière
 func GetClassList(res *map[string]map[string]interface{}, filepath string) bool {
 	return GetItemList(res, filepath)
 }
 
-// Juste un alias pour simplifier la compréhension du code au main car les fonctions marchent de la même manière
+// Juste un alias de GetItemList pour simplifier la compréhension du code au main,
+// car les fonctions marchent de la même manière
 func GetMonsterList(res *map[string]map[string]interface{}, filepath string) bool {
 	return GetItemList(res, filepath)
+}
+
+// Ouvre le fichier json 'filepath' et le converti en variable lisible par golang dans 'res'
+func GetTradesList(res *map[string]map[string]map[string]int, filepath string) bool {
+	//Retourne 0 si aucune erreur n'a été rencontré lors du dépactage du json. Sinon retourne 1
+	file, err1 := os.ReadFile(filepath)
+	if err1 != nil || json.Unmarshal(file, res) != nil {
+		return true
+	}
+	return false
 }
 
 // |========================================================|
@@ -66,7 +79,8 @@ func InitInventory() Inventory {
 	return make(Inventory)
 }
 
-// Permet l'ajout de l'item portant l'id 'itemID' à l'inventaire 'inv' avec pour quantité 'quantity'.
+// Permet l'ajout de l'item portant l'id 'itemID'
+// à l'inventaire 'inv' avec pour quantité 'quantity'.
 func AddItemToInventory(inv *Inventory, itemID string, quantity int) {
 	//quantity doit être positif
 	_, ok := (*inv)[itemID]
@@ -77,8 +91,10 @@ func AddItemToInventory(inv *Inventory, itemID string, quantity int) {
 	}
 }
 
-// Permet la suppréssion de l'item portant l'id 'itemID' à l'inventaire 'inv' avec pour quantité 'quantity'.
-// Renvoie 'true' si l'action est impossible (retirer plus d'item que le joueur en a) et sinon renvoie 'false'.
+// Permet la suppréssion de l'item portant l'id 'itemID'
+// à l'inventaire 'inv' avec pour quantité 'quantity'.
+// Renvoie 'true' si l'action est impossible (retirer plus d'item que le joueur en a)
+// et sinon renvoie 'false'.
 func RemoveItemFromInventory(inv *Inventory, itemID string, quantity int) bool {
 	//quantity doit être positif
 	//Retourne false si on a réussit à lui retirer sinon renvoie true.
@@ -94,7 +110,8 @@ func RemoveItemFromInventory(inv *Inventory, itemID string, quantity int) bool {
 	return false
 }
 
-// Renvoie la liste des items commencant par les tags 'item_types' fournies dans l'inventaire 'inv'.
+// Renvoie la liste des items commencant par les tags 'item_types'
+// fournies dans l'inventaire 'inv'.
 func getInventoryByItemType(inv Inventory, item_types []string) ([]string, []int) {
 	res := []string{}
 	res2 := []int{}
@@ -109,7 +126,8 @@ func getInventoryByItemType(inv Inventory, item_types []string) ([]string, []int
 	return res, res2
 }
 
-// Renvoie la liste des items ne commencant pas par les tags 'item_types' fournies dans l'inventaire 'inv'.
+// Renvoie la liste des items ne commencant pas par les tags 'item_types'
+// fournies dans l'inventaire 'inv'.
 func getInventoryNotItemType(inv Inventory, item_types []string) ([]string, []int) {
 	res := []string{}
 	res2 := []int{}
@@ -129,12 +147,14 @@ func getInventoryNotItemType(inv Inventory, item_types []string) ([]string, []in
 	return res, res2
 }
 
-// Renvoie la liste des items consomables ainsi qu'une autre liste contenant la quantité de chaques chacun.
+// Renvoie la liste des items consomables
+// ainsi qu'une autre liste contenant la quantité de chaques chacun.
 func GetInventoryConsumables(inv Inventory) ([]string, []int) {
 	return getInventoryByItemType(inv, []string{"CP"})
 }
 
-// Renvoie la liste des items équipables ainsi qu'une autre liste contenant la quantité de chacun.
+// Renvoie la liste des items équipables
+// ainsi qu'une autre liste contenant la quantité de chacun.
 func GetInventoryEquipable(inv Inventory) ([]string, []int) {
 	return getInventoryByItemType(inv, []string{"EC", "EA", "EL", "EB", "EW"})
 }
@@ -177,7 +197,8 @@ func InitPlayer() map[string]interface{} {
 	return res
 }
 
-// Fonction à but de test uniquement, permet l'affichage de 'player' sous la forme d'un dictionnaire.
+// Fonction à but de test uniquement.
+// Permet l'affichage de 'player' sous la forme d'un dictionnaire.
 func PrintPlayer(player map[string]interface{}) {
 	print("{\n")
 	for k, v := range player {
@@ -197,7 +218,8 @@ func AddGoldToPlayer(player *map[string]interface{}, quantity int) {
 }
 
 // Permet la supréssion d'or au joueur 'player' avec pour quantité 'quantity'.
-// Renvoie 'true' si l'action est impossible (retirer plus d'or que le joueur en a) et sinon renvoie 'false'.
+// Renvoie 'true' si l'action est impossible (retirer plus d'or que le joueur en a)
+// et sinon renvoie 'false'.
 func RemoveGoldFromPlayer(player *map[string]interface{}, quantity int) bool {
 	val := (*player)["gold"].(int)
 	if val < quantity {
@@ -221,7 +243,8 @@ func HealPlayer(player *map[string]interface{}, quantity int) {
 	}
 }
 
-// Permet d'infliger 'quantity' dégat(s) au joueur 'player', si l'on inflige plus de dégats que de PV restant au joueur, ses PV sont mits à 0.
+// Permet d'infliger 'quantity' dégat(s) au joueur 'player'.
+// Si l'on inflige plus de dégats que de PV restant au joueur, ses PV sont mits à 0.
 // Renvoie true si le joueur meurt des dégats sinon renvoie false.
 func HurtPlayer(player *map[string]interface{}, quantity int) bool {
 	if (*player)["hp"].(int)-quantity <= 0 {
@@ -242,7 +265,8 @@ func HealPlayerMana(player *map[string]interface{}, quantity int) {
 	}
 }
 
-// Permet de consomé 'quantity' mana au joueur 'player', si l'on consomme plus de mana que de mana restant au joueur, son mana est mit à 0.
+// Permet de consomé 'quantity' mana au joueur 'player'.
+// Si l'on consomme plus de mana que de mana restant au joueur, son mana est mit à 0.
 // Renvoie true si le joueur n'a plus des dégats sinon renvoie false.
 func UsePlayerMana(player *map[string]interface{}, quantity int) bool {
 	if (*player)["mana"].(int)-quantity <= 0 {
@@ -253,8 +277,8 @@ func UsePlayerMana(player *map[string]interface{}, quantity int) bool {
 	return false
 }
 
-// Permet d'infliger 'quantity' dégats au joueur 'player' chaques secondes pendant 'duration' secondes.
-// Un 'quantity' ou 'duration' inférieur ou égale à 0 arretera la fonction instantanément.
+// Permet d'infliger 'quantity' dégats au joueur 'player' chaques secondes,
+// pendant 'duration' secondes.
 func HurtPlayerDPS(player *map[string]interface{}, quantity int, duration int) {
 	if duration > 0 || quantity > 0 {
 		time.Sleep(1 * time.Second)
@@ -263,7 +287,8 @@ func HurtPlayerDPS(player *map[string]interface{}, quantity int, duration int) {
 	}
 }
 
-// Renvoie 'true' si le joueur 'playr' peut équipé un item 'equipment' en vérifiant si il en possède au moins un dans l'inventaire 'inv'.
+// Renvoie 'true' si le joueur 'player' peut équipé un item 'equipment',
+// en vérifiant si il en possède au moins un dans l'inventaire 'inv'.
 // Sinon renvoie 'false'.
 func PlayerCanEquip(
 	player map[string]interface{},
@@ -276,11 +301,17 @@ func PlayerCanEquip(
 	return false
 }
 
-// Equipe le joueur 'player' avec l'équipement 'itemID', déséquipe l'équipement actuelle dans le même emplacement si il en a déjà un.
+// Equipe le joueur 'player' avec l'équipement 'itemID'.
+// Déséquipe l'équipement actuelle dans le même emplacement si il en a déjà un.
 // Les items équipé et déséquipé sont ajouté et retirer de l'inventaire 'inv'.
-// Cette fonction à pour objectif d'être utiliser après avoir vérifier si l'item 'itemID' peut être équipé.
+// Cette fonction à pour objectif d'être utiliser après
+// avoir vérifier si l'item 'itemID' peut être équipé.
 // Applique aussi les effets des armures au joueur comme "maxhealth" et "maxmana".
-func EquipPlayerWith(player *map[string]interface{}, itemID string, inv *Inventory, itemList map[string]map[string]interface{}) {
+func EquipPlayerWith(
+	player *map[string]interface{},
+	itemID string, inv *Inventory,
+	itemList map[string]map[string]interface{},
+) {
 	slot := ""
 	switch itemID[:2] {
 	case "EC":
@@ -298,7 +329,9 @@ func EquipPlayerWith(player *map[string]interface{}, itemID string, inv *Invento
 	case "W":
 		slot = "EquipmentWeapon"
 	}
-	if (*player)[slot].(string) != "" { // Si le slot n'est pas vide, on rend l'équipement au joueur dans son inventaire puis on recalcule les stats.
+	// Si le slot n'est pas vide,
+	// on rend l'équipement au joueur dans son inventaire puis on recalcule les stats.
+	if (*player)[slot].(string) != "" {
 		AddItemToInventory(inv, (*player)[slot].(string), 1)
 		switch itemList[(*player)[slot].(string)]["type"].(string) {
 		case "maxhealth":
@@ -333,7 +366,8 @@ func EquipPlayerWith(player *map[string]interface{}, itemID string, inv *Invento
 // |				   	  Gestion Crafts                    |
 // |========================================================|
 
-// Fonction pour vérifier si le joueur 'player' peut crafter l'objet 'craftID' (vérifie les matériaux et or nécessaire au craft).
+// Fonction pour vérifier si le joueur 'player' peut crafter l'objet 'craftID'
+// (vérifie les matériaux et l'or nécessaire au craft).
 // Renvoie 'true' si le craft est possible sinon renvoie 'false'.
 func CanCraft(
 	craftID string,
@@ -358,7 +392,8 @@ func CanCraft(
 	return true
 }
 
-// Fonction renvoyant un slice (une liste) contenant l'id des objets que le joueur 'player' peut craft
+// Fonction renvoyant un slice (une liste),
+// contenant l'id des objets que le joueur 'player' peut craft
 func GetCraftableList(
 	craftlist map[string]map[string]int,
 	inv Inventory,
@@ -373,8 +408,10 @@ func GetCraftableList(
 	return res
 }
 
-// Craft l'item 'itemID' et retire les matériaux nécessaire de l'inventaire 'inv' du joueur 'player' ainsi que la quantité d'or nécessaire
-// Cette fonction à pour objectif d'être utiliser après avoir vérifier si l'item 'itemID' peut être craft.
+// Craft l'item 'itemID' et retire les matériaux nécessaire de l'inventaire 'inv'
+// du joueur 'player' ainsi que la quantité d'or nécessaire
+// Cette fonction à pour objectif d'être utiliser après
+// avoir vérifier si l'item 'itemID' peut être craft.
 func Craft(
 	craftID string,
 	craftlist map[string]map[string]int,
@@ -417,8 +454,9 @@ func GetClassNames(listclass map[string]map[string]interface{}) []string {
 // |				 Gestion ressource files                |
 // |========================================================|
 
-// Charge les images commencant par 'icon_' placé dans le dossier 'filepath' et les range dans 'imglst'.
-// Si une erreur apparai durant le chargement des images, la fonction s'arrète et renvoie 'true', sinon renvoie 'false'.
+// Charge les images commencant par 'icon_' placé dans le dossier 'filepath',
+// puis les range dans 'imglst'.Si une erreur apparai durant le chargement des images,
+// la fonction s'arrète et renvoie 'true', sinon renvoie 'false'.
 func LoadClassIcons(imglst *map[string]image.Image, filepath string) bool {
 	files, err := os.ReadDir(filepath)
 	if err != nil {
@@ -455,7 +493,8 @@ func LoadBG(imglst *map[string]image.Image, filepath string) bool {
 	return false
 }
 
-// Fonctions permettant de chargé et décodé une image en 'png' pour utilisation avec la librairie Tview.
+// Fonctions permettant de chargé et décodé une image en 'png',
+// pour utilisation avec la librairie Tview.
 func TViewMakeImg(addresse string) (image.Image, bool) {
 	IMGbyte, err := os.ReadFile(addresse)
 	graphics, err2 := png.Decode(bytes.NewReader(IMGbyte))
@@ -463,4 +502,56 @@ func TViewMakeImg(addresse string) (image.Image, bool) {
 		return nil, true
 	}
 	return graphics, false
+}
+
+// |========================================================|
+// |	              Gestion pnj marchant                  |
+// |========================================================|
+
+// Renvoie 'true' si le joueur 'player' peut acheter l'item 'itemID' auprès du vendeur 'trader'.
+// Sinon renvoie 'false'. (Qu'il n'ai pas assez d'or où que l'item n'y soit pas vendu)
+func CanBuyItemFrom(
+	player map[string]interface{},
+	tradeList map[string]map[string]map[string]int,
+	trader string,
+	itemID string,
+) bool {
+	val, ok := tradeList[trader]["sells"][itemID]
+	return ok && player["gold"].(int) >= val
+}
+
+// Renvoie 'true' si le vendeur 'trader' peut acheter l'item 'itemID' auprès du joueur 'player'.
+// Sinon renvoie 'false'.(Qu'il n'ai pas l'item sur lui ou que le vendeur n'achaite pas cette item)
+func CanSellItemTo(
+	inv map[string]int,
+	tradeList map[string]map[string]map[string]int,
+	trader string,
+	itemID string,
+) bool {
+	_, ok := tradeList[trader]["buys"][itemID]
+	return ok && inv[itemID] > 0
+}
+
+// Permet l'achat de l'item 'itemID' auprès du vendeur 'trader' par le joueur 'player'.
+func BuyItemFrom(
+	player *map[string]interface{},
+	inv *map[string]int,
+	tradeList map[string]map[string]map[string]int,
+	trader string,
+	itemID string,
+) {
+	RemoveGoldFromPlayer(player, tradeList[trader]["sells"][itemID])
+	AddItemToInventory(inv, itemID, 1)
+}
+
+// Permet la vente de l'item 'itemID' auprès du vendeur 'trader' par le joueur 'player'
+func SellItemTo(
+	player *map[string]interface{},
+	inv *map[string]int,
+	tradeList map[string]map[string]map[string]int,
+	trader string,
+	itemID string,
+) {
+	AddGoldToPlayer(player, tradeList[trader]["buys"][itemID])
+	RemoveItemFromInventory(inv, itemID, 1)
 }
