49b1a1336f4aff644a57864b5397c7a8c853194b
madchip777 <51477281+madchip777@users.noreply.github.com>
2024-10-10 15:24:17 +0200

    dev menu village
    todo forgeron et vendeur

--- src/Smenu/Smenu.go
+++ src/Smenu/Smenu.go
@@ -57,11 +57,10 @@ func SmenuRender(
 	buttonF.SetBorder(true) //.SetRect(0, 0, 22, 3)
 
 	buttonV := tview.NewButton("go to village").SetSelectedFunc(func() {
-		sceneValue = 1
 		app.Stop()
+		Svillage(classes_icons, bg_imgs, player, itemlist, inv)
 	})
 	buttonV.SetBorder(true) //.SetRect(0, 0, 22, 3)
-
 	// ++++++ flex windows ++++++
 	gaucheforet := tview.NewFlex().
 		SetDirection(tview.FlexRow)
@@ -406,4 +405,211 @@ func CreateRightPart(
 		AddItem(image, 0, 1, false).
 		AddItem(gridDownRight, 0, 1, false)
 	return Droiteflex
+}
+
+func CreateLeftPart(
+	classes_icons map[string]image.Image,
+	bg_imgs map[string]image.Image,
+	player *map[string]interface{},
+	itemlist map[string]map[string]interface{},
+	inv map[string]int,
+	imageTop map[string]image.Image,
+	imageBottom map[string]image.Image,
+	directionTop tview.Primitive,
+	directionBottom tview.Primitive,
+	nameTop string,
+	nameBottom string,
+	headerText string,
+	app *tview.Application,
+) *tview.Flex {
+
+	headergauche := tview.NewTextView().SetText(headerText)
+	headergauche.SetTextColor(tcell.ColorGhostWhite)
+	headergauche.SetTextAlign(tview.AlignCenter)
+	headergauche.SetBorder(true)
+
+	buttonTOP := tview.NewButton(nameTop).SetSelectedFunc(func() {
+		app.Stop()
+		if err4 := app.SetRoot(directionTop, true).EnableMouse(true).Run(); err4 != nil {
+			panic(err4)
+		}
+		//forgeron
+	})
+	buttonTOP.SetBorder(true)
+
+	buttonBottom := tview.NewButton(nameBottom).SetSelectedFunc(func() {
+		app.Stop()
+		if err4 := app.SetRoot(directionBottom, true).EnableMouse(true).Run(); err4 != nil {
+			panic(err4)
+		}
+		//vendeur
+	})
+	buttonBottom.SetBorder(true)
+
+	flexTOP := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(buttonBottom, 3, 1, true)
+		// futur une image
+	flexTOP.SetBorder(true)
+
+	flexBottom := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(buttonBottom, 3, 1, true)
+		// futur une image
+	flexBottom.SetBorder(true)
+
+	GaucheFlex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(headergauche, 3, 1, true).
+		AddItem(flexTOP, 0, 1, true).
+		AddItem(flexBottom, 0, 1, true)
+
+	return GaucheFlex
+}
+
+func Svillage(
+	classes_icons map[string]image.Image,
+	bg_imgs map[string]image.Image,
+	player *map[string]interface{},
+	itemlist map[string]map[string]interface{},
+	inv map[string]int,
+) {
+	app := tview.NewApplication()
+	// ============================partit droite=====================================
+
+	Droiteflex := CreateRightPart(classes_icons, player, itemlist)
+
+	//================================PARTIT GAUCHE ===============================
+
+	linkTop := Sshopvillage(classes_icons, bg_imgs, player, itemlist, inv, "forgeron")
+	linkBottom := Sshopvillage(classes_icons, bg_imgs, player, itemlist, inv, "vendeur")
+
+	Gaucheflex := CreateLeftPart(classes_icons, bg_imgs, player, itemlist, inv, bg_imgs, bg_imgs, linkTop, linkBottom,
+		"top", "bottom", "Rendre visite a votre tres cher :", app)
+
+	// ============================ CENTRE =============================
+
+	quitButton := tview.NewButton("Retour").
+		SetSelectedFunc(func() {
+			app.Stop()
+			SmenuRender(classes_icons, bg_imgs, player, itemlist, inv)
+		})
+	quitButton.SetBackgroundColor(tcell.ColorRed)
+
+	invBoutton := tview.NewButton("Inventaire").SetSelectedFunc(func() {
+		app.Stop()
+		ShowInventory(classes_icons, bg_imgs, player, itemlist, inv)
+	})
+	invBoutton.SetBorder(true)
+
+	gridCenter := tview.NewGrid().
+		SetRows(0, 0, 0, 0).
+		SetColumns(0, 0, 0, 0).
+		AddItem(quitButton, 4, 4, 1, 1, 0, 0, true).
+		AddItem(invBoutton, 4, 2, 1, 2, 0, 0, true)
+
+	textTop := tview.NewTextView().SetText("je suis un texte qui raconte une histoire tres tres interessente, telement que vous meme avez oublié pouquoi vous liser ceci")
+	textTop.SetBorder(true)
+	textTop.SetTextColor(tcell.ColorDarkRed)
+	textTop.SetTextAlign(tview.AlignCenter)
+
+	CentreTop := tview.NewFlex().
+		AddItem(textTop, 0, 1, false)
+	//build
+	Centreflex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(CentreTop, 0, 1, true).
+		AddItem(gridCenter, 0, 1, true)
+
+	// ====================== BUILD ==============================
+	Machted := tview.NewFlex().
+		SetDirection(tview.FlexColumn).
+		AddItem(Gaucheflex, 0, 1, true).
+		AddItem(Centreflex, 0, 1, true).
+		AddItem(Droiteflex, 0, 1, false)
+	if err4 := app.SetRoot(Machted, true).EnableMouse(true).Run(); err4 != nil {
+		panic(err4)
+	}
+}
+
+func Sshopvillage(
+	classes_icons map[string]image.Image,
+	bg_imgs map[string]image.Image,
+	player *map[string]interface{},
+	itemlist map[string]map[string]interface{},
+	inv map[string]int,
+	who string,
+) tview.Primitive {
+	app := tview.NewApplication()
+	// ============================partit droite=====================================
+
+	Droiteflex := CreateRightPart(classes_icons, player, itemlist)
+
+	//================================PARTIT GAUCHE ===============================
+
+	textTopgauche := tview.NewTextView()
+	textTopgauche.SetBorder(true)
+	textTopgauche.SetTextColor(tcell.ColorDarkRed)
+	textTopgauche.SetTextAlign(tview.AlignCenter)
+
+	switch who { // who is selling
+	case "vendeur":
+		//image vendeur
+		textTopgauche.SetText("hohoho, ici je vend bien de bonne chose, tout cela est t'a toi, mais seulement SI tu ne possede assez de pieces d'or hihihi")
+
+		// liste interactive vendeur
+	case "forgeron":
+		//image forgerons
+		textTopgauche.SetText("Hmfmfhmfm, bricole cassé, objet a bricolé, bienvenu dans les flamme et le fer, rien de mieux pour battre l'enfer mfmfmfm")
+
+		// liste interactive for
+	}
+
+	Gaucheflex := tview.NewFlex().
+		AddItem(textTopgauche, 0, 1, true)
+	// ============================ CENTRE =============================
+	//bottom
+	quitButton := tview.NewButton("Retour").
+		SetSelectedFunc(func() {
+			app.Stop()
+			SmenuRender(classes_icons, bg_imgs, player, itemlist, inv)
+		})
+	quitButton.SetBackgroundColor(tcell.ColorRed)
+
+	invBoutton := tview.NewButton("Inventaire").SetSelectedFunc(func() {
+		app.Stop()
+		ShowInventory(classes_icons, bg_imgs, player, itemlist, inv)
+	})
+	invBoutton.SetBorder(true)
+
+	gridCenter := tview.NewGrid().
+		SetRows(0, 0, 0, 0).
+		SetColumns(0, 0, 0, 0).
+		AddItem(quitButton, 4, 4, 1, 1, 0, 0, true).
+		AddItem(invBoutton, 4, 2, 1, 2, 0, 0, true)
+	// top
+	textTop := tview.NewTextView().SetText("je suis un texte qui raconte une histoire tres tres interessente, telement que vous meme avez oublié pouquoi vous liser ceci")
+	textTop.SetBorder(true)
+	textTop.SetTextColor(tcell.ColorDarkRed)
+	textTop.SetTextAlign(tview.AlignCenter)
+
+	CentreTop := tview.NewFlex().
+		AddItem(textTop, 0, 1, false)
+	//build
+	Centreflex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(CentreTop, 0, 1, true).
+		AddItem(gridCenter, 0, 1, true)
+
+	// ============================ build ===========================================
+	Machted := tview.NewFlex().
+		SetDirection(tview.FlexColumn).
+		AddItem(Gaucheflex, 0, 1, true).
+		AddItem(Centreflex, 0, 1, true).
+		AddItem(Droiteflex, 0, 1, false)
+
+	return Machted
+	// if err4 := app.SetRoot(Machted, true).EnableMouse(true).Run(); err4 != nil {
+	// 	panic(err4)
+	// }
 }
